36fb998c35df9a32bbf179e2cd1f9eb7186dccc7
junjiequan <a331998513@gmail.com>
2026-02-24 13:14:50 +0100

    init

--- src/elastic-search/interfaces/mappingInterface.type.ts
+++ src/elastic-search/interfaces/mappingInterface.type.ts
@@ -1,15 +1,4 @@
-import { searchType } from "./es-common.type";
-
-export interface MappingProperty {
-  type: searchType;
-  fields?: {
-    keyword: {
-      type: searchType;
-      ignore_above: number;
-    };
-  };
-  [key: string]: unknown;
-}
+export type MappingProperty = Record<string, unknown>;
 
 export interface MappingObject {
   [key: string]: MappingProperty;
