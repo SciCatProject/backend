e0714f12efc0be16db0e3a0b25e6e3b1af853377
junjiequan <a331998513@gmail.com>
2026-03-19 16:08:40 +0100

    minor text change

--- src/opensearch/utils/dataset-opensearch.utils.ts
+++ src/opensearch/utils/dataset-opensearch.utils.ts
@@ -1,16 +1,15 @@
-// It controlles which fields of the dataset document should be excluded
+// It controlles which fields of the dataset document should be included
 // when sending it to Opensearch.
 export const DATASET_OPENSEARCH_FIELDS = [
   "description",
   "datasetName",
   "isPublished",
-  "relationships",
   "ownerGroup",
   "accessGroups",
 ] as const;
 
 // OUTPUT EXAMPLE:
-// { description: 1, datasetName: 1, isPublished: 1, relationships: 1, ownerGroup: 1, accessGroups: 1 }
+// { description: 1, datasetName: 1, isPublished: 1, ownerGroup: 1, accessGroups: 1 }
 export const DATASET_OPENSEARCH_PROJECTION = Object.fromEntries(
   DATASET_OPENSEARCH_FIELDS.map((key) => [key, 1]),
 );
