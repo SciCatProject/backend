f0341b4b237400a685c59498a96b4d7984cbbfb9
junjiequan <a331998513@gmail.com>
2026-03-19 16:44:42 +0100

    add excludeExtraneousValues ot plainToInstance

--- src/opensearch/dto/dataset-opensearch.dto.ts
+++ src/opensearch/dto/dataset-opensearch.dto.ts
@@ -1,9 +1,17 @@
-import { PickType } from "@nestjs/swagger";
+import { OmitType } from "@nestjs/swagger";
+import { Expose } from "class-transformer";
+import { OutputDatasetDto } from "src/datasets/dto/output-dataset.dto";
 
-import { UpdateDatasetDto } from "src/datasets/dto/update-dataset.dto";
 import { DATASET_OPENSEARCH_FIELDS } from "src/opensearch/utils/dataset-opensearch.utils";
 
-export class DatasetOpenSearchDto extends PickType(
-  UpdateDatasetDto,
+export class DatasetOpenSearchDto extends OmitType(
+  OutputDatasetDto,
   DATASET_OPENSEARCH_FIELDS,
-) {}
+) {
+  @Expose() pid: string;
+  @Expose() description?: string;
+  @Expose() datasetName: string;
+  @Expose() isPublished?: boolean;
+  @Expose() ownerGroup: string;
+  @Expose() accessGroups: string[];
+}
