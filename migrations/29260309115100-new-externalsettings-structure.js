c4c245e606569a007d319cc231f16ad984cd616f
Abdi Mo <abdi.abdulle@outlook.dk>
2026-03-25 09:51:50 +0100

    updated migration script

--- migrations/29260309115100-new-externalsettings-structure.js
+++ migrations/29260309115100-new-externalsettings-structure.js
@@ -6,53 +6,11 @@ module.exports = {
       {
         $unset: {
           columns: "",
-          "externalSettings.columns": "",
-          "externalSettings.conditions": "",
-          "externalSettings.filters": "",
-          "externalSettings.tablesSettings": "",
-        },
-        $set: {
-          "externalSettings.fe_dataset_table_columns": [],
-          "externalSettings.fe_dataset_table_conditions": [],
-          "externalSettings.fe_dataset_table_filters": [],
-          "externalSettings.fe_proposal_table_columns": [],
-          "externalSettings.fe_proposal_table_filters": [],
-          "externalSettings.fe_sample_table_columns": [],
-          "externalSettings.fe_sample_table_conditions": [],
-          "externalSettings.fe_instrument_table_columns": [],
-          "externalSettings.fe_file_table_columns": [],
+          externalSettings: "",
         },
       },
     );
 
     console.log("Successfully migrated UserSetting structure");
-  },
-
-  async down(db, client) {
-    await db.collection("UserSetting").updateMany(
-      {},
-      {
-        $unset: {
-          "externalSettings.fe_dataset_table_columns": "",
-          "externalSettings.fe_dataset_table_conditions": "",
-          "externalSettings.fe_dataset_table_filters": "",
-          "externalSettings.fe_proposal_table_columns": "",
-          "externalSettings.fe_proposal_table_filters": "",
-          "externalSettings.fe_sample_table_columns": "",
-          "externalSettings.fe_sample_table_conditions": "",
-          "externalSettings.fe_instrument_table_columns": "",
-          "externalSettings.fe_file_table_columns": "",
-        },
-        $set: {
-          columns: "",
-          "externalSettings.columns": "",
-          "externalSettings.conditions": "",
-          "externalSettings.filters": "",
-          "externalSettings.tablesSettings": "",
-        },
-      },
-    );
-
-    console.log("Successfully reverted UserSetting structure");
   },
 };
