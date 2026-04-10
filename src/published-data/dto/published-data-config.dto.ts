7a3d825c94890d5ac735cdf269376831ac6d90a1
fpotier <frederic.potier@protonmail.com>
2026-04-10 13:36:07 +0000

    use `Record<string, unknown>` instead of `object`

--- src/published-data/dto/published-data-config.dto.ts
+++ src/published-data/dto/published-data-config.dto.ts
@@ -6,15 +6,13 @@ export class PublishedDataConfigDto {
   @ApiProperty({
     description:
       "[JSON schema](https://json-schema.org/docs) defining the structure for the metadata property of PublishedData objects.",
-    type: Object,
   })
-  metadataSchema: object;
+  metadataSchema: Record<string, unknown>;
 
   @IsObject()
   @ApiProperty({
     description:
       "[UI schema](https://jsonforms.io/docs/uischema/) to use in the frontend for metadata edition",
-    type: Object,
   })
-  uiSchema: object;
+  uiSchema: Record<string, unknown>;
 }
