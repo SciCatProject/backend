5a1008019bf840b4ccc7818bf151b67388415d51
Omkar Zade <omkar.zade@psi.ch>
2026-04-15 12:12:55 +0000

    fix checkModifiedSince for instruments

--- src/instruments/instruments.service.ts
+++ src/instruments/instruments.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Inject, Scope, NotFoundException } from "@nestjs/common";
+import {
+  Injectable,
+  Inject,
+  Scope,
+  NotFoundException,
+  PreconditionFailedException,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { FilterQuery, Model, UpdateQuery } from "mongoose";
 import { IFilters } from "src/common/interfaces/common.interface";
@@ -91,11 +97,22 @@ export class InstrumentsService {
     return this.instrumentModel.findOne(whereFilter, fieldsProjection).exec();
   }
 
-  async update(
+  async findOneAndUpdate(
     filter: FilterQuery<InstrumentDocument>,
     updateInstrumentDto: PartialUpdateInstrumentDto,
+    unmodifiedSince?: Date,
   ): Promise<Instrument | null> {
     const username = (this.request.user as JWTUser).username;
+
+    const exists = await this.instrumentModel.exists(filter).exec();
+    if (!exists) {
+      throw new NotFoundException(
+        `Instrument not found with filter: ${JSON.stringify(filter)}`,
+      );
+    }
+    if (unmodifiedSince) {
+      filter.updatedAt = { $lte: unmodifiedSince };
+    }
 
     const updatedInstrument = this.instrumentModel
       .findOneAndUpdate(
@@ -111,8 +128,8 @@ export class InstrumentsService {
       .exec();
 
     if (!updatedInstrument) {
-      throw new NotFoundException(
-        `Instrument not found with filter: ${JSON.stringify(filter)}`,
+      throw new PreconditionFailedException(
+        `Resource #${filter._id} has been modified on server since ${unmodifiedSince?.toUTCString()}`,
       );
     }
 
