9b2a5b97f6e1a4d37215b9373df896bd9adc9baf
Alex Lubbock <code@alexlubbock.com>
2026-05-08 22:23:34 +0100

    test: add regression tests for datasetId index on block schemas

---
+++ src/origdatablocks/schemas/origdatablock.schema.spec.ts
@@ -0,0 +1,10 @@
+import { OrigDatablockSchema } from "./origdatablock.schema";
+
+describe("OrigDatablockSchema", () => {
+  it("should have an index on datasetId", () => {
+    const indexFields = OrigDatablockSchema.indexes().map(
+      ([fields]) => fields,
+    );
+    expect(indexFields).toContainEqual({ datasetId: 1 });
+  });
+});
