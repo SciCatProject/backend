73d0576a8c31256cca1e32385bdef4ffff3e995b
Alex Lubbock <code@alexlubbock.com>
2026-05-08 22:59:51 +0100

    style: fix prettier formatting in origdatablock schema spec

--- src/origdatablocks/schemas/origdatablock.schema.spec.ts
+++ src/origdatablocks/schemas/origdatablock.schema.spec.ts
@@ -2,9 +2,7 @@ import { OrigDatablockSchema } from "./origdatablock.schema";
 
 describe("OrigDatablockSchema", () => {
   it("should have an index on datasetId", () => {
-    const indexFields = OrigDatablockSchema.indexes().map(
-      ([fields]) => fields,
-    );
+    const indexFields = OrigDatablockSchema.indexes().map(([fields]) => fields);
     expect(indexFields).toContainEqual({ datasetId: 1 });
   });
 });
