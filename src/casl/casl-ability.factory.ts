1bc60105b9a81b2c45ee1da94fe670bc3313933a
HayenNico <141850426+HayenNico@users.noreply.github.com>
2026-06-03 22:02:02 +0200

    extract casl type definitions to separate file

--- src/casl/casl-ability.factory.ts
+++ src/casl/casl-ability.factory.ts
@@ -1,64 +1,38 @@
 import {
   AbilityBuilder,
   ExtractSubjectType,
-  InferSubjects,
   MongoAbility,
-  MongoQuery,
   createMongoAbility,
 } from "@casl/ability";
+import { accessibleBy } from "@casl/mongoose";
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { Attachment } from "src/attachments/schemas/attachment.schema";
+import { JobConfigService } from "src/config/job-config/jobconfig.service";
 import { JWTUser } from "src/auth/interfaces/jwt-user.interface";
 import { AccessGroupsType } from "src/config/configuration";
-import { JobConfig } from "src/config/job-config/jobconfig.interface";
-import { JobConfigService } from "src/config/job-config/jobconfig.service";
+import { Attachment } from "src/attachments/schemas/attachment.schema";
 import { Datablock } from "src/datablocks/schemas/datablock.schema";
 import { DatasetClass } from "src/datasets/schemas/dataset.schema";
 import { Instrument } from "src/instruments/schemas/instrument.schema";
 import { JobClass } from "src/jobs/schemas/job.schema";
+import { JobConfig } from "src/config/job-config/jobconfig.interface";
 import { CreateJobAuth, UpdateJobAuth } from "src/jobs/types/jobs-auth.enum";
 import { Logbook } from "src/logbooks/schemas/logbook.schema";
+import { MetadataKeyClass } from "src/metadata-keys/schemas/metadatakey.schema";
+import { Opensearch } from "src/opensearch/opensearch.subject";
 import { OrigDatablock } from "src/origdatablocks/schemas/origdatablock.schema";
 import { Policy } from "src/policies/schemas/policy.schema";
 import { ProposalClass } from "src/proposals/schemas/proposal.schema";
 import { PublishedData } from "src/published-data/schemas/published-data.schema";
+import { RuntimeConfig } from "src/config/runtime-config/schemas/runtime-config.schema";
 import { SampleClass } from "src/samples/schemas/sample.schema";
-import { UserIdentity } from "src/users/schemas/user-identity.schema";
-import { UserSettings } from "src/users/schemas/user-settings.schema";
 import { User } from "src/users/schemas/user.schema";
 import { Action } from "./action.enum";
-import { RuntimeConfig } from "src/config/runtime-config/schemas/runtime-config.schema";
-import { accessibleBy } from "@casl/mongoose";
-import { MetadataKeyClass } from "src/metadata-keys/schemas/metadatakey.schema";
-import { Opensearch } from "src/opensearch/opensearch.subject";
-import { GenericHistory } from "src/common/schemas/generic-history.schema";
-
-type Subjects =
-  | string
-  | InferSubjects<
-      | typeof Attachment
-      | typeof Datablock
-      | typeof DatasetClass
-      | typeof GenericHistory
-      | typeof Instrument
-      | typeof JobClass
-      | typeof Logbook
-      | typeof MetadataKeyClass
-      | typeof Opensearch
-      | typeof OrigDatablock
-      | typeof Policy
-      | typeof ProposalClass
-      | typeof PublishedData
-      | typeof RuntimeConfig
-      | typeof SampleClass
-      | typeof User
-      | typeof UserIdentity
-      | typeof UserSettings
-    >
-  | "all";
-type PossibleAbilities = [Action, Subjects];
-type Conditions = MongoQuery;
+import {
+  Subjects,
+  PossibleAbilities,
+  Conditions
+} from "./types/casl-subjects"
 
 export type AppAbility = MongoAbility<PossibleAbilities, Conditions>;
 
