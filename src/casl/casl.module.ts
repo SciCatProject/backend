30b94eeca5f1645eafd1872090fbd2a2f62a11bd
HayenNico <141850426+HayenNico@users.noreply.github.com>
2026-06-05 08:46:01 +0200

    refactor history casl & controller

--- src/casl/casl.module.ts
+++ src/casl/casl.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CaslAbilityFactory } from "./casl-ability.factory";
 import { JobConfigModule } from "src/config/job-config/jobconfig.module";
+import { HistoryAbility } from "./abilities/history.ability";
+
 @Module({
   imports: [JobConfigModule, ConfigModule],
-  providers: [CaslAbilityFactory],
+  providers: [CaslAbilityFactory, HistoryAbility],
   exports: [CaslAbilityFactory],
 })
 export class CaslModule {}
