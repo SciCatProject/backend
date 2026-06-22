3e364105cbf1bbbd23a5afd81394aea60f2c58e9
junjiequan <a331998513@gmail.com>
2026-06-22 15:53:46 +0200

    added sse listener to watch mongodb changestream

--- src/serverSentEvent/decorators/sse.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators, UseInterceptors } from "@nestjs/common";
-import { SseEventType, SseInterceptor } from "../interceptors/sse.interceptor";
-
-export const EmitSse = (eventType: SseEventType) => {
-  return applyDecorators(UseInterceptors(SseInterceptor(eventType)));
-};
