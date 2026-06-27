6042050b93ebeee4c9649edbd2ef58dd7aa09121
HayenNico <141850426+HayenNico@users.noreply.github.com>
2026-06-27 15:28:04 +0200

    refactor runtime-config casl

---
+++ src/casl/abilities/runtime-config.ability.ts
@@ -0,0 +1,52 @@
+import {
+  AbilityBuilder,
+  ExtractSubjectType,
+  MongoAbility,
+  createMongoAbility,
+} from "@casl/ability";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AccessGroupsType } from "src/config/configuration";
+import { Action } from "../action.enum";
+import {
+  Subjects,
+  PossibleAbilities,
+  Conditions,
+} from "../types/casl-subjects";
+import { JWTUser } from "src/auth/interfaces/jwt-user.interface";
+import { RuntimeConfig } from "src/config/runtime-config/schemas/runtime-config.schema";
+
+@Injectable()
+export class RuntimeConfigAbility {
+  constructor(private configService: ConfigService) {
+    this.accessGroups =
+      this.configService.get<AccessGroupsType>("accessGroups");
+  }
+  private accessGroups;
+
+  buildAbility(user: JWTUser): MongoAbility<PossibleAbilities, Conditions> {
+    const { can, build } = new AbilityBuilder(
+      createMongoAbility<PossibleAbilities, Conditions>,
+    );
+
+    /**
+     * Any user
+     */
+    can(Action.RuntimeConfigRead, RuntimeConfig);
+
+    if (
+      user &&
+      user.currentGroups.some((g) => this.accessGroups?.admin.includes(g))
+    ) {
+      /**
+       * User belonging to ADMIN_GROUPS
+       */
+      can(Action.RuntimeConfigUpdate, RuntimeConfig);
+    }
+
+    return build({
+      detectSubjectType: (item) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
