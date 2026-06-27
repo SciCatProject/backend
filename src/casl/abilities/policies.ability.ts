5ff241f537fe23cb06a5d8fdc1dba231cff494d4
HayenNico <141850426+HayenNico@users.noreply.github.com>
2026-06-27 13:57:06 +0200

    refactor policies casl & controller

---
+++ src/casl/abilities/policies.ability.ts
@@ -0,0 +1,64 @@
+import {
+  AbilityBuilder,
+  ExtractSubjectType,
+  MongoAbility,
+  createMongoAbility,
+} from "@casl/ability";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AccessGroupsType } from "src/config/configuration";
+import { Action } from "../action.enum";
+import {
+  Subjects,
+  PossibleAbilities,
+  Conditions,
+} from "../types/casl-subjects";
+import { JWTUser } from "src/auth/interfaces/jwt-user.interface";
+import { Policy } from "src/policies/schemas/policy.schema";
+
+@Injectable()
+export class PolicyAbility {
+  constructor(private configService: ConfigService) {
+    this.accessGroups =
+      this.configService.get<AccessGroupsType>("accessGroups");
+  }
+  private accessGroups;
+
+  buildAbility(user: JWTUser): MongoAbility<PossibleAbilities, Conditions> {
+    const { can, build } = new AbilityBuilder(
+      createMongoAbility<PossibleAbilities, Conditions>,
+    );
+
+    if (
+      user &&
+      user.currentGroups.some(
+        (g) =>
+          this.accessGroups?.admin.includes(g) ||
+          this.accessGroups?.policy.includes(g),
+      )
+    ) {
+      /**
+       * User belonging to ADMIN_GROUPS or POLICY_GROUPS
+       */
+
+      can(Action.Create, Policy);
+      can(Action.Read, Policy);
+      can(Action.Update, Policy);
+    }
+
+    if (
+      user &&
+      user.currentGroups.some((g) => this.accessGroups?.delete.includes(g))
+    ) {
+      /**
+       * User belonging to DELETE_GROUPS
+       */
+      can(Action.Delete, Policy);
+    }
+
+    return build({
+      detectSubjectType: (item) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
