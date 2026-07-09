cae9f44d805aaa927876c8b0216b5ef8baeba4e1
junjiequan <a331998513@gmail.com>
2026-07-09 12:07:13 +0200

    fix: allow multiple sorts

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -389,8 +389,10 @@ export const parseOrderLimits = (
   const limitFilters: ILimitsFilterV4 = structuredClone(limits);
   if (!limits.order) return limitFilters;
   const sort: Record<string, "asc" | "desc"> = {};
-  const [field, direction] = limits.order.split(":");
-  if (direction === "asc" || direction === "desc") sort[field] = direction;
+  for (const part of limits.order.split(",")) {
+    const [field, direction] = part.split(":");
+    if (direction === "asc" || direction === "desc") sort[field] = direction;
+  }
   limitFilters.sort = sort;
   return omit(limitFilters, "order");
 };
